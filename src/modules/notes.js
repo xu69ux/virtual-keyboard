b1f22bd7583e85e0acb23b1373d81f5a0bff8864
xu <spiridonova.kseniya@gmail.com>
2023-05-02 22:01:29 +0600

    fix: fix melody of pain

--- src/modules/notes.js
+++ src/modules/notes.js
@@ -7,5 +7,5 @@ export const Cd = 554.4;
 export const D = 587.3;
 
 export const pain = [
-  B, Fd, E, G, Fd, E, Fd, E, Fd, G, G, Fd, E, B,
-  Fd, B, G, Fd, E, D, E, D, Cd, Cd, D, Cd, B];
+  B, Fd, E, G, Fd, E, Fd, E, Fd, G, G, Fd, E,
+  B, Fd, B, G, Fd, E, D, E, D, Cd, Cd, D, Cd];
